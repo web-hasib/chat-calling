33ed33d1e4fce32314f16ab9d60649ffa0ad2c12
web-hasib <hasibulislam9284@gmail.com>
2026-07-21 16:33:53 +0600

    build: generate production distribution files for backend service

--- backend-nest/dist/src/storage/cloudinary-storage.provider.d.ts
+++
@@ -1,8 +0,0 @@
-import { ConfigService } from '@nestjs/config';
-import { StorageProvider } from './storage-provider.interface';
-export declare class CloudinaryStorageProvider implements StorageProvider {
-    private configService;
-    constructor(configService: ConfigService);
-    uploadFile(file: Express.Multer.File): Promise<string>;
-    deleteFile(fileUrl: string): Promise<void>;
-}
--- backend-nest/dist/src/storage/storage-provider.interface.d.ts
+++
@@ -1,4 +0,0 @@
-export interface StorageProvider {
-    uploadFile(file: Express.Multer.File): Promise<string>;
-    deleteFile(fileUrl: string): Promise<void>;
-}
